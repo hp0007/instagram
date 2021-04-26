48fb9b35609d81653bb93fe48e965e195ebf0141
hp0007 <harshitgajjar95336@gmail.com>
2021-04-27 01:20:27 +0530

    update design

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,9 +24,7 @@ const Sidebar = (props) => {
           <div className="follows d-flex">
             <div className="total-post">
               <p>50</p>
-              <p>
-                <posts></posts>
-              </p>
+              <p>posts</p>
             </div>
             <div className="followers">
               <p>1k</p>
